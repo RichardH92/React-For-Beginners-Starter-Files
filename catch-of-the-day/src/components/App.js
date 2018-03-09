cef2c95f28119212f869f846b90668a41b69347d
Richard Hart <rhart@medallia.com>
2018-03-09 08:20:26 -0500

    14

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -2,6 +2,7 @@ import React, {Component} from "react";
 import Header from "./Header";
 import Inventory from "./Inventory";
 import Order from "./Order";
+import sampleFishes from "../sample-fishes";
 
 class App extends Component {
     state = {
@@ -17,13 +18,20 @@ class App extends Component {
         });
     }
 
+    loadSampleFishes = () => {
+        this.setState({ fishes: sampleFishes });
+    }
+
     render() {
         return (
             <div className="catch-of-the-day">
                 <div className="menu">
                     <Header tagline="Fresh Seafood Market"/>
                 </div>
-                <Inventory addFish={this.addFish}/>
+                <Inventory 
+                    addFish={this.addFish}
+                    loadSampleFishes={this.loadSampleFishes}
+                />
                 <Order/>
             </div>
         )
